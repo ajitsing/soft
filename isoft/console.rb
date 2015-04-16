5ce1569ac240ac7634adb93be48d5a851bef5c2e
ajitsingh <jeetsingh.ajit@gmail.com>
2015-04-16 21:25:23 +0530

    intractive shell for soft

---
+++ isoft/console.rb
@@ -0,0 +1,64 @@
+#!/usr/bin/env ruby
+load("#{File.dirname(__FILE__)}/../load_lib.rb")
+load("#{File.dirname(__FILE__)}/pretty_printer.rb")
+
+include PrettyPrinter
+
+def colorize(text, color_code)
+  "#{color_code}#{text}\e[0m"
+end
+
+def get_input
+  answer = ''
+  while true
+    char = STDIN.getc
+    answer += char.chr
+    break if char == "\n"
+  end
+
+  answer
+end
+
+def start_console
+  while true
+    $stdout.write 'soft 0.1> '
+    input = get_input
+    shut_down if input == "exit\n"
+
+    begin
+      compile input
+    rescue Exception => e
+      print_warning("Parsing error: #{e.message} \n")
+    end
+  end
+  system 'stty sane'
+end
+
+def compile(input)
+  tokens = Soft::Scanner.tokenize input
+  print_in :yellow, " => #{tokens}\n"
+  print_info " => #{Soft::Parser.new.to_statements tokens}\n"
+end
+
+def shut_down
+  print_warning "\nShutting down gracefully..."
+  sleep 1
+  exit
+end
+
+def shut_down_with_error(err)
+  print_warning "\nShutting down because of following error\n#{err}"
+  sleep 1
+  exit
+end
+
+def reset_terminal
+  system 'stty sane'
+end
+
+Signal.trap("INT") {
+  reset_terminal
+  shut_down
+}
+
+start_console
---
+++ isoft/pretty_printer.rb
@@ -0,0 +1,25 @@
+module PrettyPrinter
+  def colorize(text, color_code)
+    "#{color_code}#{text}\e[0m"
+  end
+
+  def red(text); colorize(text, "\e[31m"); end
+  def green(text); colorize(text, "\e[32m"); end
+  def yellow(text); colorize(text, "\e[1m\e[33m");  end
+  def console_blink(text); colorize(text, "\e[5m");  end
+  def console_bold(text); colorize(text, "\e[1m");  end
+
+  def print_info(msg, centered = false)
+    msg = msg.center(terminal_width) if centered
+    $stdout.write green(msg)
+  end
+
+  def print_warning(msg, centered = false)
+    msg = msg.center(terminal_width) if centered
+    $stdout.write red(msg)
+  end
+
+  def print_in(color, msg)
+    $stdout.write self.send(color, msg)
+  end
+end
